a63eae124cffa3162b987fea644abb57007b887c
Nicolaas1988 <nicolaas.steenekamp@gmail.com>
2024-07-30 01:25:48 +0200

    real names added

--- frontend/src/components/SelectName.jsx
+++ frontend/src/components/SelectName.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 function SelectName({ selectedName, setSelectedName }) {
-  const names = ['John', 'Doe', 'Jane', 'Smith', 'Alex'];
+  const names = ['Lynette', 'Jan', 'Klea', 'Bernadene', 'Esther', 'Andre','Nicolaas','Saskia', 'Connor','Kristin'];
 
   return (
     <select value={selectedName} onChange={(e) => setSelectedName(e.target.value)}>
